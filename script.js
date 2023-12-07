c7da16b9ee2e18302ef5745aabbe0f2fd25c12ec
Gabriella <Gabriella.broberg@live.se>
2023-12-07 19:56:24 +0100

    Successful checkbox + score

--- script.js
+++ script.js
@@ -1,124 +1,67 @@
 const questions = [
   {
-    question: "Vilka av följande är operativsystem? (du kan välja fler)",
-    type: "checkbox",
+    question: "Which is the largest animal in the world?",
     answers: [
-      { text: "Windows", correct: true },
-      { text: "Linux", correct: true },
-      { text: "Chrome", correct: false },
-      { text: "iOS", correct: true },
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is the smallest country in the world?",
+    answers: [
+      { text: "Vatican City", correct: true },
+      { text: "Bhutan", correct: false },
+      { text: "Nepal", correct: false },
+      { text: "Shri Lanka", correct: false },
+    ],
+  },
+  {
+    question: "Which is the largest desert in the world?",
+    answers: [
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: false},
+      { text: "Sahara", correct: false },
+      { text: "Antarctica", correct: true },
+    ],
+  },
+  {
+    question: "Which are countries?",
+    answers: [
+      { text: "Brazil", correct: true },
+      { text: "Australia", correct: true },
+      { text: "pear", correct: false },
+      { text: "bike", correct: false },
     ],
   },
 
   {
-    question: "HTML är ett programmeringsspråk.",
-    type: "trueFalse",
+    question: "Which are Animals?",
     answers: [
-      { text: "Sant", correct: false },
-      { text: "Falskt", correct: true },
-    ],
-  },
-  {
-    question: "Git används för versionshantering av kod.",
-    type: "trueFalse",
-    answers: [
-      { text: "Sant", correct: true },
-      { text: "Falskt", correct: false },
-    ],
-  },
-  {
-    question: "Vilket företag utvecklade JavaScript?",
-    type: "multipleChoice",
-    answers: [
-      { text: "Microsoft", correct: false },
-      { text: "Oracle", correct: false },
-      { text: "Google", correct: false },
-      { text: "Netscape", correct: true },
-    ],
-  },
-  {
-    question: "Vad är DOM i JavaScript?",
-    type: "multipleChoice",
-    answers: [
-      { text: "Data Output Management", correct: false },
-      { text: "Document Object Model", correct: true },
-      { text: "Digital Object Model", correct: false },
-      { text: "Dynamic Output Method", correct: false },
-    ],
-  },
-
-  {
-    question: "Vilket av följande är ett programmeringsspråk?",
-    type: "multipleChoice",
-    answers: [
-      { text: "brailleScript", correct: false },
-      { text: "JavaScript", correct: true },
-      { text: "HTML", correct: false },
-      { text: "CSS", correct: false },
-    ],
-  },
-
-  {
-    question: "Vilken funktion används för att skriva ut text i JavaScript?",
-    type: "multipleChoice",
-    answers: [
-      { text: "print", correct: false },
-      { text: "write", correct: false },
-      { text: "console.log", correct: true },
-      { text: "output", correct: false },
-    ],
-  },
-  {
-    question: "CSS står för 'Central Style Sheet'.",
-    type: "multipleChoice",
-    answers: [
-      { text: "Sant", correct: false },
-      { text: "Falskt", correct: true },
-    ],
-  },
-  {
-    question: "Vilka av följande är frontend-ramverk eller bibliotek?",
-    type: "checkbox",
-    answers: [
-      { text: "React", correct: true },
-      { text: "Express", correct: false },
-      { text: "Django", correct: false },
-      {
-        text: "Vue",
-        correct: true,
-      },
-    ],
-  },
-  {
-    question: "Vad gör kommandot 'git pull?",
-    type: "multipleChoice",
-    answers: [
-      { text: "Skickar ändringar till det lokala arkivet", correct: false },
-      { text: "Skapar en ny gren", correct: false },
-      { text: "Tar bort den nuvarande grenen", correct: false },
-      {
-        text: "Hämtar och integrerar ändringar från ett fjärrarkiv",
-        correct: true,
-      },
+      { text: "Kiwi", correct: false },
+      { text: "Horse", correct: true },
+      { text: "Dog", correct: true },
+      { text: "bike", correct: false },
     ],
   },
 ];
 
-
 const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
+const answerCheckboxes = document.getElementById("answer-checkboxes");
 const nextButton = document.getElementById("next-btn");
-const answerCheckbox = document.getElementById("answerCheckbox");
 
-let currentQuestionIndex = 0;
+//Store the question index and score
+
+let currentQuestionIndex = 0; //Index will start from 0
 let score = 0;
 
+//When starting the quiz it will restart with 0
 function startQuiz() {
   currentQuestionIndex = 0;
   score = 0;
   nextButton.innerHTML = "Next";
-  resetState();
-  showQuestion();
+  showQuestion(); //set the scoe 0 and set the text next in the button
 }
 
 function showQuestion() {
@@ -127,158 +70,124 @@ function showQuestion() {
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-  answerCheckbox.innerHTML = '';
+  currentQuestion.answers.forEach((answer) => {
+    const container = document.createElement("div");
+    container.classList.add("answer-container");
 
-  if (currentQuestion.type === "checkbox") {
-    currentQuestion.answers.forEach((answer) => {
-      const checkboxLabel = document.createElement("label");
-      checkboxLabel.innerHTML = `
-        <input type="checkbox" name="${answer.text}" value="${answer.text}"> ${answer.text}
-      `;
-      answerCheckbox.appendChild(checkboxLabel);
-    });
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = answer.text;
+    checkbox.name = "answers";
+    checkbox.value = answer.text;
+    checkbox.dataset.answer = answer.value; // Assuming there is an 'value' property in your answer object
+    checkbox.classList.add("checkbox");
 
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach((checkbox) => {
-      checkbox.addEventListener("input", selectAnswer);
-    });
+    const answerText = document.createElement("span");
+    answerText.innerHTML = answer.text;
 
-  } else {
-    currentQuestion.answers.forEach((answer) => {
-      const button = document.createElement("button");
-      button.innerHTML = answer.text;
-      button.classList.add("btn");
-      answerButtons.appendChild(button);
-      if (answer.correct) {
-        button.dataset.correct = answer.correct;
-      }
-      button.addEventListener("click", selectAnswer);
-    });
-  }
+    container.appendChild(checkbox);
+    container.appendChild(answerText);
+    answerCheckboxes.appendChild(container);
+
+    if (answer.correct) {
+      checkbox.dataset.correct = answer.correct;
+    }
+
+    checkbox.addEventListener("change", selectAnswer);
+  });
 }
+
 
 function resetState() {
-  questionElement.style.color = "";
   nextButton.style.display = "none";
-  while (answerButtons.firstChild) {
-    answerButtons.removeChild(answerButtons.firstChild);
+  while (answerCheckboxes.firstChild) {
+    answerCheckboxes.removeChild(answerCheckboxes.firstChild);
   }
 }
 
-function selectAnswer(e) {
-  const selectedElement = e.target;
+/*function selectAnswer(e) {
+  const selectedCheckbox = e.target;
+  const isCorrect = selectedCheckbox.dataset.correct === "true";
 
-  if (selectedElement.tagName === 'INPUT' && selectedElement.type === 'checkbox') {
-    const isCorrect = selectedElement.checked;
-    const answerText = selectedElement.value;
-
-    const selectedAnswer = questions[currentQuestionIndex].answers.find(answer => answer.text === answerText);
-
-    if (isCorrect && selectedAnswer.correct) {
-      selectedElement.classList.add("correct");
-    } else {
-      selectedElement.classList.add("incorrect");
-    }
-
-    // Kolla om det är sista checkboxen och om alla är korrekta
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const lastCheckbox = checkboxes[checkboxes.length - 1];
-    if (selectedElement === lastCheckbox) {
-      const selectedCheckboxAnswers = getSelectedCheckboxAnswers();
-      const allCorrect = selectedCheckboxAnswers.every(answer => answer.correct);
-
-      if (allCorrect) {
-        score++;
-      }
-    }
-  } else {
-    // Hantera befintlig logik för knappar
-    const isCorrect = selectedElement.dataset.correct === "true";
-
+  if (selectedCheckbox.checked) {
     if (isCorrect) {
-      selectedElement.classList.add("correct");
+      selectedCheckbox.classList.add("correct");
       score++;
     } else {
-      selectedElement.classList.add("incorrect");
+      selectedCheckbox.classList.add("incorrect");
     }
-
-    Array.from(answerButtons.children).forEach((button) => {
-      button.disabled = true;
-    });
+  } else {
+    selectedCheckbox.classList.remove("correct", "incorrect");
   }
 
-  console.log('Current Score:', score);
+
+
+  Array.from(answerCheckboxes.querySelectorAll('.checkbox')).forEach((checkbox) => {
+    //checkbox.disabled = true;
+  });
+
+  nextButton.style.display = "block";
+} */
+
+
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+let selectedAnswers = [];
+
+function selectAnswer(e) {
+  const selectedCheckbox = e.target;
+
+  if (selectedCheckbox.checked) {
+    selectedCheckbox.classList.add("selected");
+    selectedAnswers.push(selectedCheckbox.value);
+  } else {
+    selectedCheckbox.classList.remove("selected");
+    selectedAnswers = selectedAnswers.filter(value => value !== selectedCheckbox.value);
+  }
+
   nextButton.style.display = "block";
 }
 
-
 function handleNextButton() {
-  currentQuestionIndex++;
+  // Check correctness only when the user clicks "Next"
+  const currentQuestion = questions[currentQuestionIndex];
+  const correctAnswers = currentQuestion.answers
+    .filter(answer => answer.correct)
+    .map(answer => answer.text);
 
+  const allCorrectSelected = selectedAnswers.length === correctAnswers.length &&
+    selectedAnswers.every(value => correctAnswers.includes(value));
+
+  if (allCorrectSelected) {
+    score++;
+  }
+  console.log(score); // Log the score after each question
+
+  currentQuestionIndex++;
   if (currentQuestionIndex < questions.length) {
     showQuestion();
   } else {
     showScore();
   }
+
+  // Reset selected answers for the next question
+  selectedAnswers = [];
 }
 
 
-function getSelectedCheckboxAnswers() {
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  const selectedAnswers = [];
 
-  checkboxes.forEach((checkbox) => {
-    const answerText = checkbox.value;
-
-    // Kolla om det finns fler frågor
-    if (currentQuestionIndex < questions.length) {
-      const selectedAnswer = questions[currentQuestionIndex].answers.find(answer => answer.text === answerText);
-      selectedAnswers.push(selectedAnswer);
+nextButton.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else {
+        startQuiz();
     }
-  });
-
-  // Ta bort dubbletter från de valda svaren baserat på texten
-  const uniqueSelectedAnswers = Array.from(new Set(selectedAnswers.map(answer => answer.text)))
-    .map(text => selectedAnswers.find(answer => answer.text === text));
-
-  return uniqueSelectedAnswers;
-}
-
-
-
-function showScore() {
-  resetState();
-  const totalQuestions = questions.length;
-  const percentage = (score / totalQuestions) * 100;
-
-  let resultText = `Du klarade ${score} av ${totalQuestions} frågor! `;
-  let resultClass = "";
-
-  if (percentage < 50) {
-    resultText += "Underkänt :( !";
-    resultClass = "red";
-  } else if (percentage >= 50 && percentage <= 75) {
-    resultText += "Bra :)";
-    resultClass = "orange";
-  } else {
-    resultText += "Riktigt bra jobbat :D";
-    resultClass = "green";
-  }
-
-  questionElement.innerHTML = resultText;
-  questionElement.style.color = resultClass;
-  questionElement.classList.add(resultClass);
-
-  nextButton.innerHTML = "Play Again";
-  nextButton.style.display = "block";
-}
-
-nextButton.addEventListener("click", () => {
-  if (currentQuestionIndex < questions.length) {
-    handleNextButton();
-  } else {
-    startQuiz();
-  }
-});
+})
 
 startQuiz();
