b425d8112ae9f25a26ee8289cfb7599f477f0a00
Gabriella <Gabriella.broberg@live.se>
2023-12-12 10:20:28 +0100

    added comments to JS, changes in CSS

--- script.js
+++ script.js
@@ -1,120 +1,123 @@
 const questions = [
-
   {
-    question: "Vilka av följande är operativsystem? (du kan välja fler)",
-    type: "checkbox", 
+    question:
+      "I filmen 'Unbreakable' spelar Samuel L. Jackson en karaktär med sköra ben och smeknamnet 'Mr. Glass'. Vilken färg är ofta förknippad med hans karaktär?",
+    type: "multipleChoice",
     answers: [
-      { text: "Windows", correct: true },
-      { text: "Linux", correct: true },
-      { text: "Chrome", correct: false },
-      { text: "iOS", correct: true },
+      { text: "Neon Pink", correct: false },
+      { text: "Lila", correct: true },
+      { text: "Orange", correct: false },
+      { text: "Svart", correct: false },
     ],
   },
 
   {
-    question: "HTML är ett programmeringsspråk.",
-    type: "trueFalse",
+    question:
+      "Vad är den dominerande färgen i albumomslaget för Taylor Swifts '1989'?",
+    type: "multipleChoice",
     answers: [
-      { text: "Sant", correct: false },
-      { text: "Falskt", correct: true },
+      { text: "Orange", correct: false },
+      { text: "Peach", correct: true },
+      { text: "Rosa", correct: false },
+      { text: "Svart", correct: false },
     ],
   },
   {
-    question: "Git används för versionshantering av kod.",
+    question:
+      "Vilken filmregissör är känd för att använda starka neonfärger i sina filmer, som 'Blade Runner'?",
+    type: "multipleChoice",
+    answers: [
+      { text: "Christopher Nolan", correct: false },
+      { text: "Quentin Tarantino", correct: false },
+      { text: "Ridley Scott", correct: true },
+      { text: "Martin Scorsese", correct: false },
+    ],
+  },
+  {
+    question:
+      "Vilken av följande kändisar är känd för att ha en varumärkesfärg i neonorange?",
+    type: "multipleChoice",
+    answers: [
+      { text: "Kanye West", correct: true },
+      { text: "Kim Kardashian", correct: false },
+      { text: "Drake", correct: false },
+      { text: "Taylor Swift", correct: false },
+    ],
+  },
+  {
+    question:
+      "Vilken artist är känd för att ha lila som en av sina signaturfärger?",
+    type: "multipleChoice",
+    answers: [
+      { text: "Ariana Grande", correct: false },
+      { text: "Bruno Mars", correct: false },
+      { text: "Lizzo", correct: true },
+      { text: "Shawn Mendes", correct: false },
+    ],
+  },
+
+  {
+    question:
+      "Prince, även känd som 'The Purple One', bar ofta lila kläder under sina konserter.",
     type: "trueFalse",
     answers: [
       { text: "Sant", correct: true },
       { text: "Falskt", correct: false },
     ],
   },
-  {
-    question: "Vilket företag utvecklade JavaScript?",
-    type: "multipleChoice",
-    answers: [
-      { text: "Microsoft", correct: false },
-      { text: "Oracle", correct: false},
-      { text: "Google", correct: false },
-      { text: "Netscape", correct: true },
-    ],
-  },
-  {
-    question: "Vad är DOM i JavaScript?",
-    type: "multipleChoice",
-    answers: [
-      { text: "Data Output Management", correct: false },
-      { text: "Document Object Model", correct: true },
-      { text: "Digital Object Model", correct: false },
-      { text: "Dynamic Output Method", correct: false },
-    ],
-  },
 
   {
-    question: "Vilket av följande är ett programmeringsspråk?",
-    type: "multipleChoice",
-    answers: [
-      { text: "brailleScript", correct: false },
-      { text: "JavaScript", correct: true },
-      { text: "HTML", correct: false },
-      { text: "CSS", correct: false },
-    ],
-  },
-
-  {
-    question: "Vilken funktion används för att skriva ut text i JavaScript?",
-    type: "multipleChoice",
-    answers: [
-      { text: "print", correct: false },
-      { text: "write", correct: false },
-      { text: "console.log", correct: true },
-      { text: "output", correct: false },
-    ],
-  },
-  {
-    question: "CSS står för 'Central Style Sheet'.",
-    type: "multipleChoice",
+    question: "Madonna är känd för att ha en signaturfärg i neonrosa.",
+    type: "trueFalse",
     answers: [
       { text: "Sant", correct: false },
       { text: "Falskt", correct: true },
-
     ],
   },
   {
-    question: "Vad gör kommandot 'git pull?",
-    type: "multipleChoice",
+    question: "Vilka av följande TV-serier har lila som en framträdande färg i sina titelkrediter?",
+    type: "checkbox",
     answers: [
-      { text: "Skickar ändringar till det lokala arkivet", correct: false },
-      { text: "Skapar en ny gren", correct: false },
-      { text: "Tar bort den nuvarande grenen", correct: false },
-      { text: "Hämtar och integrerar ändringar från ett fjärrarkiv", correct: true },
+      { text: "Breaking Bad", correct: false },
+      { text: "Stranger Things", correct: false },
+      { text: "The Crown", correct: true },
+      { text: "Game of Thrones", correct: true },
     ],
   },
   {
-    question: "Vad gör funktionen 'map' i JavaScript?",
-    type: "multipleChoice",
+    question:
+      "Vilka av följande filmer har neonpeach som en framträdande färg i sina scenografi? ",
+    type: "checkbox",
     answers: [
-      { text: "Lägger till ett nytt element i en array", correct: false },
-      { text: "Tar bort ett element från en array", correct: false },
-      { text: "Itererar över varje element i en array och tillämpar en funktion", correct: true },
-      { text: "Sorterar elementen i en array i stigande ordning", correct: false },
+      { text: "La La Land", correct: false },
+      { text: "Call Me By Your Name", correct: true },
+      { text: "The Grand Budapest Hotel", correct: false },
+      { text: "Her", correct: true },
     ],
-  }
-  
-
-
+  },
+  {
+    question:
+      "Vilka av följande artister har använt neonrosa som en dominerande färg i sina musikvideor? ",
+    type: "checkbox",
+    answers: [
+      { text: "Katy Perry", correct: true },
+      { text: "Sia", correct: false },
+      { text: "Nicki Minaj", correct: true },
+      { text: "John Legend", correct: false },
+    ],
+  },
 ];
 
+// Get the HTML elements representing the quiz components
 const questionElement = document.getElementById("question");
 const answerCheckboxes = document.getElementById("answer-checkboxes");
 const nextButton = document.getElementById("next-btn");
 
-
-
-
-//Store the question index and score
-
+// Store the index of the current question and the user's score
 let currentQuestionIndex = 0; //Index will start from 0
 let score = 0;
 
+// Arrays to store selected answers and user responses
 let selectedAnswers = [];
 let userResponses = [];
 
@@ -132,17 +135,18 @@ function startQuiz() {
   // Hide the feedback container initially
   document.getElementById("feedback-container").style.display = "none";
 
-
+  // Show the first question
   showQuestion();
 }
 
-
+// Function to display a question
 function showQuestion() {
   resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
+  // Loop through answers and create checkboxes
   currentQuestion.answers.forEach((answer) => {
     const container = document.createElement("div");
     container.classList.add("answer-container");
@@ -166,78 +170,73 @@ function showQuestion() {
       checkbox.dataset.correct = answer.correct;
     }
 
+    // Add event listener to checkboxes
     checkbox.addEventListener("change", selectAnswer);
   });
 }
 
-
+// Function to reset the state of the quiz
 function resetState() {
-  
   nextButton.style.display = "none";
   questionElement.style.color = ""; // Reset text color
   while (answerCheckboxes.firstChild) {
     answerCheckboxes.removeChild(answerCheckboxes.firstChild);
   }
-  
 }
 
-
-
-
+// Function to display the user's score and feedback
 function showScore() {
   resetState();
   const totalQuestions = questions.length;
   const percentage = (score / totalQuestions) * 100;
 
-  let resultText = `Du fick ${score} rätt av ${totalQuestions}!`;
+  let resultText = `Du fick ${score} av ${totalQuestions} rätt!`;
   let resultClass = "";
 
-
-    // Determine result class based on the percentage
-    if (percentage < 50) {
-      resultText += "<br>Underkänt :( !";
-      resultClass = "red";
-    } else if (percentage >= 50 && percentage <= 75) {
-      resultText += "<br>Bra :)";
-      resultClass = "orange";
-    } else {
-      resultText += "<br>Riktigt bra jobbat :D";
-      resultClass = "green";
-    }
+  // Determine result class based on the percentage
+  if (percentage < 50) {
+    resultText += "<br>Underkänt :( !";
+    resultClass = "red";
+  } else if (percentage >= 50 && percentage <= 75) {
+    resultText += "<br>Bra :)";
+    resultClass = "orange";
+  } else {
+    resultText += "<br>Riktigt bra jobbat :D";
+    resultClass = "green";
+  }
 
   // Show the feedback container
   document.getElementById("feedback-container").style.display = "block";
 
-  const resultContainer = document.createElement('div');
-  resultContainer.classList.add('result-container');
+  const resultContainer = document.createElement("div");
+  resultContainer.classList.add("result-container");
 
   // Display correct/incorrect information for each question
   userResponses.forEach((response, index) => {
-    const resultItem = document.createElement('div');
-    resultItem.classList.add('result-item');
+    const resultItem = document.createElement("div");
+    resultItem.classList.add("result-item");
 
-    resultItem.innerHTML = `<br>${index + 1}: <span style="color: ${response.correct ? 'green' : 'red'}">${response.correct ? 'Rätt' : 'Fel'}</span>`;
-
+    resultItem.innerHTML = `<br>${index + 1}: <span style="color: ${
+      response.correct ? "green" : "red"
+    }">${response.correct ? "Rätt" : "Fel"}</span>`;
 
     // Display the selected answers
     if (response.userSelections.length > 0) {
-      resultItem.innerHTML += `<br>Du svarade: ${response.userSelections.join(', ')}`;
+      resultItem.innerHTML += `<br>Du svarade: ${response.userSelections.join(
+        ", "
+      )}`;
     }
 
     // Display the correct answers
-    resultItem.innerHTML += `<br>Korrekt: ${questions[index].answers
-      .filter(answer => answer.correct)
-      .map(answer => answer.text)
-      .join(', ')}`;
+    resultItem.innerHTML += `<br>Facit: ${questions[index].answers
+      .filter((answer) => answer.correct)
+      .map((answer) => answer.text)
+      .join(", ")}`;
 
     resultContainer.appendChild(resultItem);
   });
 
   document.getElementById("feedback-container").appendChild(resultContainer);
-
-
-
-
 
   document.getElementById("question").innerHTML = resultText;
   document.getElementById("question").style.color = resultClass;
@@ -247,9 +246,7 @@ function showScore() {
   nextButton.style.display = "block";
 }
 
-
-
-
+// Function to handle the user's answer selection
 function selectAnswer(e) {
   const selectedCheckbox = e.target;
 
@@ -258,29 +255,31 @@ function selectAnswer(e) {
     selectedAnswers.push(selectedCheckbox.value);
   } else {
     selectedCheckbox.classList.remove("selected");
-    selectedAnswers = selectedAnswers.filter(value => value !== selectedCheckbox.value);
+    selectedAnswers = selectedAnswers.filter(
+      (value) => value !== selectedCheckbox.value
+    );
   }
 
   nextButton.style.display = "block";
 }
-
+// Function to handle the "Next" button click
 function handleNextButton() {
   // Check correctness only when the user clicks "Next"
   const currentQuestion = questions[currentQuestionIndex];
   const correctAnswers = currentQuestion.answers
-    .filter(answer => answer.correct)
-    .map(answer => answer.text);
+    .filter((answer) => answer.correct)
+    .map((answer) => answer.text);
 
-  const allCorrectSelected = selectedAnswers.length === correctAnswers.length &&
-    selectedAnswers.every(value => correctAnswers.includes(value));
+  const allCorrectSelected =
+    selectedAnswers.length === correctAnswers.length &&
+    selectedAnswers.every((value) => correctAnswers.includes(value));
 
   if (allCorrectSelected) {
     score++;
   }
   console.log(score); // Log the score after each question
 
-
-  userResponses.push({ 
+  userResponses.push({
     question: currentQuestion.question,
     userSelections: selectedAnswers,
     correct: allCorrectSelected,
@@ -293,31 +292,28 @@ function handleNextButton() {
     showScore();
   }
 
-
-
   // Reset selected answers for the next question
   selectedAnswers = [];
 }
 
-
-
-nextButton.addEventListener("click", () =>{
-    if(currentQuestionIndex < questions.length){
-        handleNextButton();
-    } else {
-        startQuiz();
-    }
-})
+// Event listener for the "Next" button
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
 
 startQuiz();
 
+// Event listener for the dark/light mode toggle button
 const toggleModeBtn = document.getElementById("toggle-mode-btn");
 const body = document.body;
 
 toggleModeBtn.addEventListener("click", toggleMode);
 
+// Function to toggle between dark and light mode
 function toggleMode() {
   body.classList.toggle("light-mode");
 }
-
-
